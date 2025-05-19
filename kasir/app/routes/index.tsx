afa8b5cdc5039f1ffc25090bbb9203c7eafec94d
Opjasson <azmitheusos333@gmail.com>
2025-05-19 19:35:49 +0700

    frontend : add ubah barang route

--- kasir/app/routes/index.tsx
+++ kasir/app/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Kasir, ManageBarang } from "../pages";
+import { Kasir, ManageBarang, Ubahbarang } from "../pages";
 
 const Route = () => {
     const Stack = createStackNavigator();
@@ -10,7 +10,7 @@ const Route = () => {
             <Stack.Screen name="kasir" component={Kasir} />
             <Stack.Screen name="manage-barang" component={ManageBarang} />
             <Stack.Screen name="tambah-barang" component={ManageBarang} />
-            <Stack.Screen name="ubah-barang" component={ManageBarang} />
+            <Stack.Screen name="ubah-barang" component={Ubahbarang} />
         </Stack.Navigator>
     );
 };
