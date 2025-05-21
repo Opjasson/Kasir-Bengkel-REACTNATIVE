39f8c6f1a6271fb033550180d626e4539eea0e1a
Opjasson <azmitheusos333@gmail.com>
2025-05-21 16:36:33 +0700

    backend : add database config

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,9 @@
 import express from "express";
-import appTest from "./routes/routeTest.js";
+
 const app = express();
 
-const getco = (req, res) => {
-    res.status(200).json({ msg: "hallo" });
-};
 app.use(express.json());
-app.use(appTest);
+
 
 app.listen(5000, (error) => {
     if (error) {
