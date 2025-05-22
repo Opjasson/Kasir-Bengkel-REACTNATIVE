d57771a53ad1c2a4fd60f677cbecee8661a5daeb
Opjasson <azmitheusos333@gmail.com>
2025-05-22 07:37:24 +0700

    backend : add deleteBarangById  route

--- backend/routes/barangRoute.js
+++ backend/routes/barangRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createBarang, getBarang, getBarangById } from "../controllers/barangController.js";
+import { createBarang, deleteBarangById, getBarang, getBarangById } from "../controllers/barangController.js";
 
 const route = express.Router();
 
 route.get("/barang", getBarang);
 route.get("/barang/:id", getBarangById);
 route.post("/barang", createBarang);
+route.delete("/barang/:id", deleteBarangById);
 
 export default route;
