b02593ee148bb41dbaf82b898e5ef15c26a67e90
Opjasson <azmitheusos333@gmail.com>
2025-05-22 07:22:30 +0700

    backend : add route getBarangById

--- backend/routes/barangRoute.js
+++ backend/routes/barangRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBarang, getBarang } from "../controllers/barangController.js";
+import { createBarang, getBarang, getBarangById } from "../controllers/barangController.js";
 
 const route = express.Router();
 
 route.get("/barang", getBarang);
+route.get("/barang/:id", getBarangById);
 route.post("/barang", createBarang);
 
 export default route;
