c6b21cbd5e69a98f2b7167b25e02adfba14328eb
Opjasson <azmitheusos333@gmail.com>
2025-05-22 07:11:54 +0700

    backend : setting env

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
 import express from "express";
 import barang from "./models/barangModel.js";
 import barang_Route from "./routes/barangRoute.js";
+import dotenv from "dotenv";
 
 // (async() => {
 //     await barang.sync()
 // })()
 
+dotenv.config();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(barang_Route);
 
-app.listen(5000, (error) => {
+app.listen(process.env.PORT, (error) => {
     if (error) {
         console.log(error);
     } else {
-        console.log("server running on port 5000");
+        console.log(`server running on http://localhost:${process.env.PORT}`);
     }
 });
