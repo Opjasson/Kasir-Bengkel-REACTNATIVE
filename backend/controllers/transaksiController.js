abd904cfbe08f77beaec55eb71db94b06b82dd06
Opjasson <azmitheusos333@gmail.com>
2025-05-25 16:14:47 +0700

    backend : add attributes "totalHarga" getAllTransaksi

--- backend/controllers/transaksiController.js
+++ backend/controllers/transaksiController.js
@@ -13,7 +13,7 @@ export const addTransaksi = async (req, res) => {
 export const getAllTransaksi = async (req, res) => {
     try {
         const response = await transaksiModel.findAll({
-            attributes: ["uuid"],
+            attributes: ["uuid","totalHarga"],
             include: [
                 {
                     model: cartModel,
