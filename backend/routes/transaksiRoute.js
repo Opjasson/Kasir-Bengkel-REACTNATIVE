fd132d9cfe4936087b6b2a2b9e568452a0ede554
Opjasson <azmitheusos333@gmail.com>
2025-05-25 15:28:35 +0700

    backend : add gettransaksiById route

--- backend/routes/transaksiRoute.js
+++ backend/routes/transaksiRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { addTransaksi, getAllTransaksi } from "../controllers/transaksiController.js"
+import { addTransaksi, getAllTransaksi, getTransaksiByUuid } from "../controllers/transaksiController.js"
 
 const route = express.Router()
 
 route.get("/transaksi", getAllTransaksi)
+route.get("/transaksi/:id", getTransaksiByUuid)
 route.post("/transaksi", addTransaksi)
 
 export default route;
