b7a9dd367b149b5f079b757383545c81980d3d4f
Opjasson <azmitheusos333@gmail.com>
2025-05-26 20:41:35 +0700

    backend : add response data transaksi in controller addTransaksi

--- backend/controllers/transaksiController.js
+++ backend/controllers/transaksiController.js
@@ -4,8 +4,8 @@ import transaksiModel from "../models/transaksiModel.js";
 
 export const addTransaksi = async (req, res) => {
     try {
-        await transaksiModel.create();
-        res.status(200).json({ msg: "transaksi berhasil dibuat" });
+      const response = await transaksiModel.create();
+        res.status(200).json({ response });
     } catch (error) {
         req.status(400).json({ msg: error.message });
     }
