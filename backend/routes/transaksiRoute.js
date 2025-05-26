97f121a8d0706ecfc252830f3377345c6904838b
Opjasson <azmitheusos333@gmail.com>
2025-05-26 20:33:22 +0700

    backend : add route update transaksi controller

--- backend/routes/transaksiRoute.js
+++ backend/routes/transaksiRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { addTransaksi, getAllTransaksi, getTransaksiByUuid } from "../controllers/transaksiController.js"
+import { addTransaksi, getAllTransaksi, getTransaksiByUuid, updateTransaksi } from "../controllers/transaksiController.js"
 
 const route = express.Router()
 
 route.get("/transaksi", getAllTransaksi)
 route.get("/transaksi/:id", getTransaksiByUuid)
 route.post("/transaksi", addTransaksi)
+route.patch("/transaksi/:id", updateTransaksi)
 
 export default route;
