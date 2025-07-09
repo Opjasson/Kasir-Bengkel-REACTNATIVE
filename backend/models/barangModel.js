1be9a617b393b686895b3b1bf6c5bf5ca3a0fe70
Opjasson <azmitheusos333@gmail.com>
2025-07-09 18:59:26 +0700

    backend : add kolom harga beli in modelBarang

--- backend/models/barangModel.js
+++ backend/models/barangModel.js
@@ -3,27 +3,34 @@ import db from "../config/database.js";
 import { INTEGER, Sequelize, STRING } from "sequelize";
 
 const barang = db.define("barang", {
-    nama : {
-        type : STRING,
-        allowNull : false,
-        validate: {
-            notEmpty: true,
-        }
-    },
-    harga : {
-        type : INTEGER,
+    nama: {
+        type: STRING,
         allowNull: false,
         validate: {
-            notEmpty: true
-        }
+            notEmpty: true,
+        },
     },
-    stok : {
-        type : INTEGER,
-        allowNull : false,
-        validate : {
-            notEmpty : true
-        }
-    }
+    harga_jual: {
+        type: INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    harga_beli: {
+        type: INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    stok: {
+        type: INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
 });
 
 export default barang;
